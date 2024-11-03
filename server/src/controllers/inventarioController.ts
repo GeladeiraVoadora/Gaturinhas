77f97411f770de63ca0ae21d797b919440e641bb
Rônald Macário <ronaldmacario13@gmail.com>
2024-11-03 20:04:25 -0300

    Add InventarioController for handling inventory retrieval

---
+++ server/src/controllers/inventarioController.ts
@@ -0,0 +1,28 @@
+// inventário do usuário (Pacotes e figurinhas. Talvez moedas no front)
+import { IInventoryService } from "../services/IInventoryService";
+import { Inventario } from "../types/inventoryType";
+
+export default class InventarioController {
+  constructor(private inventoryService: IInventoryService) {}
+
+  async findInv(req: any, res: any) {
+    try {
+      const { invId } = req.params;
+      
+      const inventario = await this.inventoryService.findInventoryById(Number(invId)) as Inventario;
+
+      if (!inventario) {
+        return res.json({ error: "inventário não encontrado" });
+      }
+
+      const gaturinhas = inventario.gat_prod.map((gp) => ({
+        prodId: gp.prodId,
+        ...gp.gat,
+      }));
+
+      return res.json(gaturinhas);
+    } catch (error) {
+      return res.json({ error });
+    }
+  }
+}
