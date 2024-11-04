fdad842e55831b0c999cfca8afebdf102d8f16b5
Rônald Macário <ronaldmacario13@gmail.com>
2024-11-04 08:01:12 -0300

    Remove DailyPController and update routing to include new routes for pac, trade, and usuario

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,6 +4,9 @@ import { gatexRouter } from "./gatexRouter";
 import { albumRouter } from "./albumRouter";
 import { coinsRouter } from "./coinsRouter";
 import { gaturinhasRouter } from "./gaturinhasRouter";
+import { pacRouter } from "./stickerPackRoute"
+import { tradeRouter } from "./tradeRoute"
+import { usuarioRouter } from "./usuarioRoutes";
 
 const mainRouter = Router();
 
@@ -12,5 +15,8 @@ mainRouter.use("/api", gatexRouter);
 mainRouter.use("/api", coinsRouter);
 mainRouter.use("/api", albumRouter);
 mainRouter.use("/api", gaturinhasRouter);
+mainRouter.use("/api", pacRouter);
+mainRouter.use("/api", tradeRouter);
+mainRouter.use("/api", usuarioRouter);
 
 export default mainRouter;
